2a39d55f7f96e3559735849916bbaeb6f578f451
ssss10000 <ssss10000@163.com>
2020-10-30 11:45:22 +0800

    Authorization Code Flow 实例 完成

--- MvcClient/Controllers/HomeController.cs
+++ MvcClient/Controllers/HomeController.cs
@@ -2,21 +2,57 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using MvcClient.Models;
 
 namespace MvcClient.Controllers
 {
+    
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            //var client = new HttpClient();
+            //var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            //if (disco.IsError)
+            //{
+            //    throw new Exception(disco.Error);
+            //}
+
+            //var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            //client.SetBearerToken(accessToken);
+            //var response = await client.GetAsync("http://localhost:5001/identity");
+            //if (!response.IsSuccessStatusCode)
+            //{
+            //    throw new Exception(response.ReasonPhrase);
+            //}
+            //var content = await response.Content.ReadAsStringAsync();
+            //return View("index", content);
             return View();
         }
 
-        public IActionResult Privacy()
+        [Authorize]
+        public async Task<IActionResult> Privacy()
         {
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            var idToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+            //var authorizationCode = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.Code);
+
+            ViewData["accessToken"] = accessToken;
+            ViewData["idToken"] = idToken;
+            ViewData["refreshToken"] = refreshToken;
+
             return View();
         }
 
@@ -24,6 +60,13 @@ namespace MvcClient.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+;
         }
     }
 }
--- MvcClient/Startup.cs
+++ MvcClient/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,6 +37,32 @@ namespace MvcClient
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //jwt的claim关闭 
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            //身份认证
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+              .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.Authority = "http://localhost:5000";
+                options.RequireHttpsMetadata = false;
+                options.ClientId = "mvc client";
+                options.ClientSecret =  "mvc secret";
+                options.SaveTokens = true;
+                options.ResponseType = "code";
+
+                options.Scope.Clear();
+                options.Scope.Add("api1");
+                options.Scope.Add(OidcConstants.StandardScopes.OpenId);
+                options.Scope.Add(OidcConstants.StandardScopes.Profile);
+                options.Scope.Add(OidcConstants.StandardScopes.OfflineAccess);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +79,9 @@ namespace MvcClient
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            app.UseAuthentication();
+
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
